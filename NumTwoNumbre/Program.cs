9e2f12c9d9af14f94476e9b328f7ccef7b32fc03
adam_ogesta <adam_ogesta@yahoo.com>
2017-09-25 20:07:29 +0200

    Add project files.

---
+++ NumTwoNumbre/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumTwoNumbre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            a = 3;
+            b = 4;
+            c = a + b;
+            Console.WriteLine("3 plus 4 equals : " + c.ToString());
+            Console.ReadKey();
+        }
+    }
+}
